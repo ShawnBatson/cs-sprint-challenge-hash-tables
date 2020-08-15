3890aa5f39939705c05fdae187e997fddb597621
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-08-14 20:32:23 -0500

    finished MVP

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -11,17 +11,18 @@ def reconstruct_trip(tickets, length):
         box[x.source] = x.destination
     # make a starting point, it must be "NONE"
     start = box["NONE"]  # set the start
-    route = [start] # set the result
-    count = 0 # set the count
-    current = start # set the current
-    
+    route = [start]  # set the result
+    count = 0  # set the count
+    current = start  # set the current
+
     for _ in box:  # open the box
         for source, destination in box.items():  # for the source and destination
-            if count > 0: # if the count is 0 (tier 1)
+            if count > 0:  # if the count is 0 (tier 1)
                 if current == source and destination != "NONE":  # if current is the source and destintaion is not None
                     route.append(destination)  # append the destination
-                    current = box[source] # change the current to the new source
-            count += 1 # add 1 (tier 1) 
+                    # change the current to the new source
+                    current = box[source]
+            count += 1  # add 1 (tier 1)
     route.append("NONE")
 
     return route

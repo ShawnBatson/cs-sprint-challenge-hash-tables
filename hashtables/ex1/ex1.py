ada246d2d5cf398ba5bf91858005c4df137c6f7f
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-08-14 19:37:30 -0500

    finished 3, my pylint/autopep is messing up horribly since my windows update, so I'm fixing that instead of stretching

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,18 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    box = {}
+    result = None
+    for iterable, weight in enumerate(weights): # enumerate the list, to make it easier
+        box[iterable] = weight # make the dictionary.
+    for key, value in box.items(): # loop over all  
+        current = value # set a current
+        for index, weight in box.items(): # loop through the box
+            if key != index:  # if the key is not the index.
+                if length != 1: # also if the length is not 1
+                    if current + weight == limit: # if the current weight and the next weight equals the limit
+                        if key > index: # check to see if the left or right is bigger and set accordingly
+                            result = [key, index]
+                        else:
+                            result = [index, key]
 
-    return None
+    return result
+
